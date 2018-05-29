9df465533603624bb0a6cff003d68fdaf290441f
Emmit <FenrirETS@gmail.com>
2018-05-29 18:33:18 -0500

    Completed the repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-var repeatString = function() {
-
+var repeatString = function(word, times) {
+  let string = ''
+  if (times <0)
+    return 'ERROR'
+  for (let i = 0; i < times; i++) {
+    string += word
+  }
+  return string
 }
 
 module.exports = repeatString
